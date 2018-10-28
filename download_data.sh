15ad592700833333c38fd677827d54d6d92788ea
Jervis <jervismuindi@yahoo.co.uk>
2018-10-28 16:47:46 -0700

    Add instructions for downlaading dataset

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # This is a script to download Google Quick Draw dataset.
-echo "Hello World"
+
+echo "Downloading quickdraw dataset from Google. This will take a moment ..."
+mkdir -p data/numpy_bitmap
+gsutil -m cp gs://quickdraw_dataset/full/simplified/*.ndjson .
+
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/*.ndjson data/numpy_bitmap
+echo "Downloading done."

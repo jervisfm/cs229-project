8be719409370b679828c8267fb954dedcad653e1
Jervis <jervismuindi@yahoo.co.uk>
2018-10-28 20:02:17 -0700

    Finish up code for splitting dataset into train/dev/test splits. We now do this as we load the data for each category to ensure good balance between the three splits. This is especially import since our data is not shuffled and sorted in category order

--- massageData.py
+++ massageData.py
@@ -38,32 +38,59 @@ class massageData():
 		self.folder = folder
 		self.X = np.zeros((0, BITMAP_DIM))
 		self.Y = []
+
+		# Split into train / test / dev. We use a split ratio of 80% train, 10% dev, 10% test.
+		# To get the 3 pieces, we'll do the splits as follows:
+		# - Split dataset into 80/20 train/eval to get train set.
+		# - Split eval piece 50/50 to get dev/test sets which each makes 10% of the overall data.
+		#
+		# Further, to ensure that the split is done uniformly across all the various categories, we
+		# will do the split for each categories (e.g. airplane) as we load it. This avoids any
+		# potential issues train/test/dev splits not being fairly balanced.
+
+		self.X_train = np.zeros((0, BITMAP_DIM))
+		self.y_train = []
+
+		self.X_dev = np.zeros((0, BITMAP_DIM))
+		self.y_dev = []
+
+		self.X_test= np.zeros((0, BITMAP_DIM))
+		self.y_test = []
+
+		random_seed = 42
 		for filename in os.listdir(self.folder):
 			fullpath = self.folder + filename
 			# use filename as Y label.
 			name = filename[:-4]
 			x = np.load(fullpath)
 			y = [name] * x.shape[0]
+			# Full dataset.
 			self.X = np.concatenate((self.X, x), axis=0)
 			self.Y += y
+
+			# Split into train/test/dev. See also comment above.
+			X_train, X_eval, y_train, y_eval = train_test_split(x, y, test_size=0.2,random_state=random_seed)
+			X_dev, X_test, y_dev, y_test = train_test_split(X_eval, y_eval, test_size=0.5, random_state=random_seed)
+
+			# Train split.
+			self.X_train = np.concatenate((self.X_train, X_train), axis=0)
+			self.y_train += y_train
+
+			# Dev split.
+			self.X_dev = np.concatenate((self.X_dev, X_dev), axis=0)
+			self.y_dev += y_dev
+
+			# Test split.
+			self.X_test = np.concatenate((self.X_test, X_test), axis=0)
+			self.y_test += y_test
+
+
+
+		# Convert into NP Arrays.
 		self.Y = np.array(self.Y)
-
-		# Split into train / test / dev. We use a split ratio of 80% train, 10% dev, 10% test.
-		# To get the 3 pieces, we'll do the splits as follows:
-		# - Split dataset into 80/20 train/eval to get train set.
-		# - Split eval piece 50/50 to get dev/test sets which each makes 10% of the overall data.
-
-		random_seed = 42
-		self.X_train, self.X_eval, self.y_train, self.y_eval = train_test_split(self.X, self.Y, test_size=0.2,
-																				random_state=random_seed)
-
-
-		self.X_dev, self.X_test, self.y_dev, self.y_test = train_test_split(self.X_eval, self.y_eval, test_size=0.5,
-
-
-																			random_state=random_seed)
-
-
+		self.y_train = np.array(self.y_train)
+		self.y_test = np.array(self.y_test)
+		self.y_dev = np.array(self.y_dev)
 
 		print("All: ", self.X.shape)
 		print("train: ", self.X_train.shape)

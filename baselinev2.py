58fb1ec216258de1f5b6799aa2aa634dfddc937d
minhphu2020 <741redwood@gmail.com>
2018-11-11 17:29:12 -0800

    baselinev2

--- baselinev2.py
+++ baselinev2.py
@@ -1,6 +1,7 @@
 import time
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 import massageData
 
@@ -9,10 +10,12 @@ def main():
     X_train, Y_train = dataGetter.getTrain()
     X_dev, Y_dev = dataGetter.getDev()
     clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X_train, Y_train)
-    print ('Prediction: ', clf.predict(X_dev))
+    Y_dev_prediction = clf.predict(X_dev)
+    print ('Prediction: ', Y_dev_prediction)
     print ('Actual Label: ', Y_dev)
     print ('Accuracy: ', clf.score(X_dev, Y_dev))
-    
+    print ("Confusion matrix: ", confusion_matrix(Y_dev, Y_dev_prediction))
+
 
 if __name__ == '__main__':
     main()

089dc3baa705e25c0b194dd05460d9b8ac56d99f
Jervis <jervismuindi@yahoo.co.uk>
2018-11-17 15:40:46 -0800

    Add flags so that generated confusion matrixs have experiment names

--- baselinev2.py
+++ baselinev2.py
@@ -6,37 +6,70 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
+
+import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('max_steps', 100, 'Number of steps to run trainer.')
+flags.DEFINE_string('data_folder', 'data/numpy_bitmap/', 'Directory which has training data to use')
+flags.DEFINE_string('experiment_name', None, 'Name for the experiment. Useful to tagging files')
+
 import massageData
 import utils
 
-class_file_name = 'class_names_50class_baselinev2'
-confusion_file_name = 'confusion_matrix_50class_baselinev2'
+class_file_name = 'class_names_baselinev2_lr'
+confusion_file_name = 'confusion_matrix_baselinev2_lr'
+
+def get_suffix_name():
+    return "_" + FLAGS.experiment if FLAGS.experiment_name else ""
+
+def get_class_filename():
+    suffix_name = get_suffix_name()
+    return "{}{}".format(class_file_name, suffix_name)
+
+def get_confusion_matrix_filename():
+    suffix_name = get_suffix_name()
+    return "{}{}".format(confusion_file_name, suffix_name)
 
 def run():
-    dataGetter = massageData.massageData()
+    print("folder = ", FLAGS.data_folder)
+
+    dataGetter = massageData.massageData(folder=FLAGS.data_folder)
+
     X_train, Y_train = dataGetter.getTrain() #TODO: feature extractions
     X_dev, Y_dev = dataGetter.getDev()
+    start_time_secs = time.time()
     print("Starting Logistic Regression training ...")
     clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial', verbose=10, n_jobs=-1).fit(X_train, Y_train)
     print("Training done.")
+    end_time_secs = time.time()
+    training_duration_secs = end_time_secs - start_time_secs
     Y_dev_prediction = clf.predict(X_dev)
-    print ('Prediction: ', Y_dev_prediction)
-    print ('Actual Label: ', Y_dev)
-    print ('Accuracy: ', clf.score(X_dev, Y_dev))
+
+    accuracy = clf.score(X_dev, Y_dev)
+    experiment_result_string = ""
+    experiment_result_string += "Prediction: {}".format(Y_dev_prediction)
+    experiment_result_string += "Actual Label: {}".format(Y_dev)
+    experiment_result_string += "Acurracy: {}".format(accuracy)
+    experiment_result_string += "Training time(secs): {}".format(training_duration_secs)
+    print(experiment_result_string)
+
     class_names = utils.get_label(Y_dev)
     confusion = confusion_matrix(Y_dev, Y_dev_prediction, labels=class_names)
     print ("Confusion matrix: ", confusion)
-    pickle.dump(class_names, open("class_names_lr", 'wb'))
-    pickle.dump(confusion, open("confusion_matrix_nclass_lr", 'wb'))
+    pickle.dump(class_names, open(get_class_filename(), 'wb'))
+    pickle.dump(confusion, open(get_confusion_matrix_filename(), 'wb'))
     
 def main():
-    class_names = pickle.load(open(class_file_name, 'rb'))[:10]
-    confusion = pickle.load(open(confusion_file_name, 'rb'))[:10,:10]
+    class_names = pickle.load(open(get_class_filename(), 'rb'))[:10]
+    confusion = pickle.load(open(get_confusion_matrix_filename(), 'rb'))[:10,:10]
     utils.create_confusion_matrices(class_names, confusion)
 
 #TODO: want to scale this up to 10 classes: add more file, compare runtime
 
 
 if __name__ == '__main__':
-    #run()
-    main()
+    run()
+    #main()

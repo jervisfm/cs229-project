48d4eb06250a6f8960e512bff7361d70527dc28e
Jervis <jervismuindi@yahoo.co.uk>
2018-11-20 21:43:26 -0800

    Update downloading to full dataset path

--- download_fulldataset.sh
+++ download_fulldataset.sh
@@ -3,6 +3,6 @@
 # This is a script to download Google Quick Draw dataset.
 
 echo "Downloading quickdraw dataset from Google. This will take a moment ..."
-mkdir -p data/numpy_bitmap
-gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/*.npy data/numpy_bitmap
+mkdir -p data/numpy_bitmap_fulldataset
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/*.npy data/numpy_bitmap_fulldataset
 echo "Downloading done."

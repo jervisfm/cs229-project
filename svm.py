eed85dde8d81a2816aa7ad6304e2cc239fcce7c2
Connie Xiao <connie3422@gmail.com>
2018-12-06 22:38:47 -0800

    adding flags

--- svm.py
+++ svm.py
@@ -7,9 +7,44 @@ import pickle
 import massageData
 import utils
 
+
+import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('max_iter', 100, 'Number of steps to run trainer.')
+flags.DEFINE_string('data_folder', 'data/numpy_bitmap/', 'Directory which has training data to use. Must have / at end.')
+flags.DEFINE_string('results_folder', 'results/', 'Folder to store result outputs from run.')
+flags.DEFINE_string('experiment_name', None, 'Name for the experiment. Useful to tagging files')
+
+class_file_name = 'class_names_baselinev2_lr'
+confusion_file_name = 'confusion_matrix_baselinev2_lr'
+
+def get_suffix_name():
+    return "_" + FLAGS.experiment_name if FLAGS.experiment_name else ""
+
+def get_class_filename():
+    suffix_name = get_suffix_name()
+    filename = "{}{}".format(class_file_name, suffix_name)
+    return os.path.join(FLAGS.results_folder, filename)
+
+def get_confusion_matrix_name():
+    return "{}{}".format(confusion_file_name, get_suffix_name())
+
+def get_confusion_matrix_filename():
+    suffix_name = get_suffix_name()
+    filename  = "{}{}".format(confusion_file_name, suffix_name)
+    return os.path.join(FLAGS.results_folder, filename)
+
+def get_experiment_report_filename():
+    suffix_name = get_suffix_name()
+    filename =  "{}{}".format("baselinev2_lr_results", suffix_name)
+    return os.path.join(FLAGS.results_folder, filename)
+
 def kernel_svm(**args):
     print(args)
-    dataGetter = massageData.massageData()
+    dataGetter = massageData.massageData(folder=FLAGS.data_folder)
     X_train, Y_train = dataGetter.getTrain()
     X_dev, Y_dev = dataGetter.getDev()
     

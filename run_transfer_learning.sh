c5fb7fa33d4d31a843cad69ed0f2b3be1d1784d5
Jervis <jervismuindi@yahoo.co.uk>
2018-12-09 23:28:35 +0000

    Update expt for transfer learning retry

--- run_transfer_learning.sh
+++ run_transfer_learning.sh
@@ -6,20 +6,21 @@
 # Run first experiment on 3 classes.
 
 set -x
-MAX_ITER=100
+MAX_ITER=20
 echo "Running transfer learning experiments on 3 classes!"
 
 # echo "Transfer learning with MobileNet"
 # python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=MobileNet --experiment_name="transfer_learning_with_MobileNet_3_classes" "$@"
 
-echo "Transfer learning with InceptionV3"
-python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=InceptionV3 --experiment_name="transfer_learning_with_InceptionV3_3_classes" "$@"
-
 echo "Transfer learning with ResNet50"
-python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=ResNet50 --experiment_name="transfer_learning_with_ResNet50_3_classes" "$@"
+python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=ResNet50 --experiment_name="transfer_learning_with_ResNet50_3_classes"  "$@"
 
 echo "Transfer learning with VGG19"
 python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=VGG19 --experiment_name="transfer_learning_with_VGG19_3_classes" "$@"
+
+echo "Transfer learning with InceptionV3"
+python3 transfer_learning.py --max_iter=$MAX_ITER --data_folder=data/numpy_bitmap_3/ --transfer_model=InceptionV3 --experiment_name="transfer_learning_with_InceptionV3_3_classes" --tune_source_model=True "$@"
+
 
 # All done
 echo "All done!"
